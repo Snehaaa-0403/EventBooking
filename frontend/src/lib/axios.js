431ac8789dceb8b0455d314031a4b16c40b0be74
Sneha <snehaaa0403@gmail.com>
2026-08-11 02:01:43 +0530

    Fixed axios URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/useAuthStore.js"
 
 const api = axios.create({
-    baseURL: "https://eventbooking-api-8z1v.onrender.com", 
+    baseURL: "https://eventbooking-api-8z1v.onrender.com/api", 
     headers: {
         'Content-Type': 'application/json',
     }
